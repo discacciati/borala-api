36e6afa6c30fa2a4ccdc7903243924315b2206e3
suellykarine <95307379+suellykarine@users.noreply.github.com>
2022-09-05 16:35:57 -0300

    feat/model_review

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    rating = models.IntegerField()
+
+    event = models.ForeignKey(
+        "events.Event", on_delete=models.CASCADE, related_name="reviews"
+    )
+
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="reviews"
+    )
