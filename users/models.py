782fb2b85888f301363521c5bf3400dc7472106a
esterfrazao <tafnisbonbon@gmail.com>
2022-09-05 16:20:13 -0300

    build: add user default model to settings

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
+import uuid
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    id = models.UUIDField(default=uuid.uuid4(), editable=False, primary_key=True)
