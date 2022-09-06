15158482a37b64945a3a796142cdd919e5391be5
Acauan Nascimento <acauan.nascimento@hotmail.com(none)>
2022-09-06 01:00:41 -0300

    feat: model_lineup

--- line_up/models.py
+++ line_up/models.py
@@ -1,3 +1,21 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class LineUp(models.Model):
+
+    id = models.UUIDField(default=uuid.uuid4(), primary_key=True, editable=False)
+    title = models.CharField(max_length=100, null=False)
+    hour = models.TimeField(null=False)
+    description = models.TextField()
+    price = models.FloatField(default=0)
+    id_event = models.UUIDField(default=uuid.uuid4)
+    talent = models.TextField()
+
+    event = models.ForeignKey(
+        "events.Event", on_delete=models.CASCADE, related_name="lineup"
+    )
+
+    def __repr__(self) -> str:
+        return f"Line-Up({self.title} - [{self.hour}])"
