23ffd2244c0ca7b15a67b75af12c3b4611253020
raniery <raniery@pop-os.localdomain>
2022-09-06 16:37:27 -0300

    feat: event deletion tests

---
+++ events/tests/test_delete.py
@@ -0,0 +1,51 @@
+from rest_framework.test import APITestCase, APIClient
+from rest_framework.views import status
+from rest_framework.authtoken.models import Token
+
+from users.models import User
+from events.models import Event
+
+class DeleteEventTest(APITestCase):
+    fixtures = ["borala_rest_api.json"]
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.event      = Event.objects.all()[0]
+        cls.admin_user = User.objects.get(is_superuser=True)
+        cls.user       = User.objects.get(is_staff=False)
+
+        cls.client = APIClient()
+    
+    def test_should_not_accept_non_admin_user(self):
+        token,_ = Token.objects.get_or_create(user_id=self.user.id)
+
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+        response      = self.client.delete(f"/api/users/{self.event.id}/")
+        response_dict = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)        
+        self.assertIn('detail', response_dict.keys())
+
+    def test_should_not_accept_invalid_token(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token 1234')
+
+        response      = self.client.delete(f"/api/users/{self.event.id}/")
+        response_dict = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)        
+        self.assertIn('detail', response_dict.keys())
+    
+    def test_should_delete_event(self):
+        token,_ = Token.objects.get_or_create(user_id=self.admin_user.id)
+
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+        response = self.client.delete(f"/api/users/{self.event.id}/")
+        
+        try:
+            User.objects.get(id=self.event.id)
+        except:
+            self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
