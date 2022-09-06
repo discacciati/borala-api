54b397d110a90436f994d6e743e6b1c90b8483d6
suellykarine <95307379+suellykarine@users.noreply.github.com>
2022-09-06 11:07:00 -0300

    feat/serializer and views

--- reviews/views.py
+++ reviews/views.py
@@ -1,13 +1,34 @@
 from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
+from reviews.mixin import serializerByMethodMixin
 from reviews.models import Review
-from reviews.serializers import ReviewSerializer
+from reviews.permissions import CustomProductPermission
+from reviews.serializers import ReviewDetailSerializer, ReviewSerializer
 
 
-class ReviewView(generics.ListCreateAPIView):
+class ReviewView(serializerByMethodMixin, generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    serializer_map = {
+        "GET": ReviewSerializer,
+        "POST": ReviewSerializer,
+    }
+
+    # DUVIDA
+    #  def perform_create(self, serializer):
+    #     serializer.save()
 
 
 class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
-    ...
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [CustomProductPermission]
+
+    queryset = Review.objects.all()
+    serializer_class = ReviewDetailSerializer
+
+    lookup_url_kwarg = "review_id"
