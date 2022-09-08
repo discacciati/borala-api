e284900255ccea69e7a6afe5fd9b75ed1deb7b00
Renata <re_juraski@hotmail.com>
2022-09-08 16:37:29 -0300

    feat: user_detail_serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,5 @@
+from dataclasses import fields
+
 from rest_framework import serializers
 
 from users.models import User
@@ -17,4 +19,22 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class UserDetailSerializer(serializers.ModelSerializer):
-    ...
+    class Meta:
+        model = User
+        fields = '__all__'
+        read_only_fields = ['is_promoter', 'is_superuser']
+        extra_kwargs = {'password': {'write_only': True}}
+
+
+    def update(self, instance: User, validated_data: dict):
+
+        if 'password' in validated_data.keys():
+            password_data = validated_data.pop('password')
+            instance.objects.set_password(password_data)
+
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+
+        instance.save()
+
+        return super().update(instance, validated_data)
