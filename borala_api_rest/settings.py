2cd0c3916db469efdadfa17dbf1eef5ed2b9cc13
esterfrazao <tafnisbonbon@gmail.com>
2022-09-09 14:27:26 -0300

    feat: add allowed_hosts

--- borala_api_rest/settings.py
+++ borala_api_rest/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://git.heroku.com/borala-api-rest.git", "localhost"]
 
 
 # Application definition
