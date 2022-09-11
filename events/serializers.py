b72720f0ba64ca9c2be1ed7afb4048c3d55a4027
raniery <raniery@pop-os.localdomain>
2022-09-11 17:35:45 -0300

    fix: Event categories are now shown properly on list route

--- events/serializers.py
+++ events/serializers.py
@@ -11,8 +11,9 @@ from .models import Event
 
 
 class EventSerializer(serializers.ModelSerializer):
-    categories = CategorySerializer(read_only=True)
+    categories = CategorySerializer(read_only=True, many=True)
     address = AddressSerializer(read_only=True)
+    line_up = LineupSerializer(read_only=True, many=True)
 
     class Meta:
         model = Event
@@ -23,7 +24,7 @@ class EventSerializer(serializers.ModelSerializer):
 class EventDetailedSerializer(serializers.ModelSerializer):
     categories = CategorySerializer(many=True)
     address = AddressSerializer()
-    line_up = LineupSerializer(read_only=True)
+    line_up = LineupSerializer(read_only=True, many=True)
 
     class Meta:
         model = Event
