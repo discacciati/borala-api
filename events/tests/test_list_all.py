ecfb9660f03b19c832afc57372eaedf04c9bface
esterfrazao <tafnisbonbon@gmail.com>
2022-09-12 11:12:14 -0300

    refactor: add last line blank space

--- events/tests/test_list_all.py
+++ events/tests/test_list_all.py
@@ -1,28 +1,35 @@
-from rest_framework.test import APITestCase, APIClient
+from events.models import Event
+from rest_framework.test import APIClient, APITestCase
 from rest_framework.views import status
 
-from events.models import Event
 
 class EventListTest(APITestCase):
-    fixtures = ["user-fixture.json", "event-fixture.json", "address-fixture.json", "category-fixture.json"]
+    fixtures = [
+        "user-fixture.json",
+        "event-fixture.json",
+        "address-fixture.json",
+        "category-fixture.json",
+    ]
 
     @classmethod
     def setUpTestData(cls):
         cls.events_list = Event.objects.all()
-        cls.events_len  = len(cls.events_list)
+        cls.events_len = len(cls.events_list)
 
         cls.client = APIClient()
-    
+
     def test_should_list_all_events(self):
-        response      = self.client.get("/api/events/")
+        response = self.client.get("/api/events/")
         response_list = response.json()
-        response_dict = {response_list[i]["id"]:resp for i,resp in enumerate(response_list)}
+        response_dict = {
+            response_list[i]["id"]: resp for i, resp in enumerate(response_list)
+        }
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response_list), self.events_len)
 
         for event in self.events_list:
-            id_string     = str(event.id)
+            id_string = str(event.id)
             response_data = response_dict[id_string]
 
             self.assertEqual(response_data["id"], id_string)
