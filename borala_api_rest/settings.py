3ef92e4c4b3cdb9d459b69b5f12025fa6299e5da
raniery <raniery@pop-os.localdomain>
2022-09-12 21:55:19 -0300

    feat:generated docs with spectacular

--- borala_api_rest/settings.py
+++ borala_api_rest/settings.py
@@ -46,6 +46,7 @@ DJANGO_APPS = [
 THIRD_PARTY_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
+    'drf_spectacular',
 ]
 
 MY_APPS = [
@@ -172,4 +173,13 @@ if DATABASE_URL:
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 5,
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'BoraLa API',
+    'DESCRIPTION': 'O MELHOR HAPPY HOUR DO BRASIL, AQUI NOlogoNav',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
 }
