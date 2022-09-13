b13e6ec29caa421b634db113c2b899afcd8078d7
Eliane Discacciati <discacciati@gmail.com>
2022-09-12 23:00:28 -0300

    fix:address_model_with_frontend

--- addresses/models.py
+++ addresses/models.py
@@ -7,10 +7,10 @@ class Address(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     state = models.CharField(max_length=2)
     city = models.CharField(max_length=50)
-    postal_code = models.CharField(max_length=10, blank=True)
-    street = models.CharField(max_length=50)
-    district = models.CharField(max_length=50)
-    number = models.PositiveIntegerField()
+    postal_code = models.CharField(max_length=10, blank=True, null=True)
+    street = models.CharField(max_length=50, blank=True, null=True)
+    district = models.CharField(max_length=50, blank=True, null=True)
+    number = models.PositiveIntegerField(null=True, blank=True)
 
     def __repr__(self) -> str:
         return f"Adress({self.street} n°{self.number}, {self.city}-{self.state})"
